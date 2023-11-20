a5c70991238ca25e7e31e0863df906080596336d
고형철 <Administrator@304-5>
2023-11-20 16:00:20 +0900

    1120 / base

---
+++ src/main/java/com/nowon/cho/HarmonyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nowon.cho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowon/cho/HarmonyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowon.cho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
